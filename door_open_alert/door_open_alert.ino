2e3863a76021ad7b7a30e9775c4e6fe1b6ee1002
mashal02 <mashfaque.bscs20seecs@seecs.edu.pk>
2022-05-22 23:21:48 +0500

    Alert through LED

--- door_open_alert/door_open_alert.ino
+++ door_open_alert/door_open_alert.ino
@@ -1,9 +1,86 @@
+#include <Arduino.h>
+#include <WiFi.h>
+// Set GPIOs for LED and reedswitch
+const int reedSwitch = 13;
+const int led = 2; //optional
+
+// Detects whenever the door changed state
+bool changeState = false;
+
+// Holds reedswitch state (1=opened, 0=close)
+bool state;
+String doorState;
+
+// Auxiliary variables (it will only detect changes that are 1500 milliseconds apart)
+unsigned long previousMillis = 0; 
+const long interval = 1500;
+
+const char* host = "maker.ifttt.com";
+const char* apiKey = "ikOgwLc8dw_eIpH18MD-jF79rUL5GHYSqtFodg5Tilf";
+
+// Runs whenever the reedswitch changes state
+ICACHE_RAM_ATTR void changeDoorStatus() {
+  Serial.println("State changed");
+  changeState = true;
+}
+
 void setup() {
-  // put your setup code here, to run once:
+
+  pinMode(reedSwitch, INPUT_PULLUP);
+  state = digitalRead(reedSwitch);
+
+  // Set LED state to match door state
+  pinMode(led, OUTPUT);
+  digitalWrite(led, !state);
+  
+  // Set the reedswitch pin as interrupt, assign interrupt function and set CHANGE mode
+  attachInterrupt(digitalPinToInterrupt(reedSwitch), changeDoorStatus, CHANGE);
+
+  Serial.begin(115200);
+  WiFi.begin("Bajwa residence", "70315o9b");
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+void loop() {
+  if (changeState){
+    unsigned long currentMillis = millis();
+    if(currentMillis - previousMillis >= interval) {
+      previousMillis = currentMillis;
+      // If a state has occured, invert the current door state   
+      state = !state;
+      if(state) {
+        doorState = "closed";
+      }
+      else{
+        doorState = "open";
+      }
+      digitalWrite(led, !state);
+      changeState = false;
+      Serial.println(state);
+      Serial.println(doorState);
+/*
+      //Send email
+      Serial.print("connecting to ");
+      Serial.println(host);
+      WiFiClient client;
+      const int httpPort = 80;
+      if (!client.connect(host, httpPort)) {
+        Serial.println("connection failed");
+        return;
+      }
+
+      String url = "/trigger/door_status/with/key/";
+      url += apiKey;
+
+      Serial.print("Requesting URL: ");
+      Serial.println(url);
+      client.print(String("POST ") + url + " HTTP/1.1\r\n" +
+                     "Host: " + host + "\r\n" + 
+                     "Content-Type: application/x-www-form-urlencoded\r\n" + 
+                     "Content-Length: 13\r\n\r\n" +
+                     "value1=" + doorState + "\r\n");
+                     */
+    }  
+  }
 }
--- wifi_connection/wifi_connection.ino
+++ wifi_connection/wifi_connection.ino
@@ -1,22 +1,22 @@
-
-#include "WiFi.h"
- 
-const char* ssid = "Bajwa Residance";
-const char* password =  "70315o9b";
- 
+#include <Arduino.h>
+#include <WiFi.h>
 void setup() {
- 
   Serial.begin(115200);
- 
-  WiFi.begin(ssid, password);
- 
+  WiFi.begin("Bajwa residence", "70315o9b");
+  // Wait for wifi to be connected
+  uint32_t notConnectedCounter = 0;
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.println("Connecting to WiFi..");
+      delay(100);
+      Serial.println("Wifi connecting...");
+      notConnectedCounter++;
+      if(notConnectedCounter > 150) { // Reset board if not connected after 5s
+          Serial.println("Resetting due to Wifi not connecting...");
+          ESP.restart();
+      }
   }
- 
-  Serial.println("Connected to the WiFi network");
- 
+  Serial.print("Wifi connected, IP address: ");
+  Serial.println(WiFi.localIP());
 }
- 
-void loop() {}
+void loop() {
+  // put your main code here, to run repeatedly:
+}
